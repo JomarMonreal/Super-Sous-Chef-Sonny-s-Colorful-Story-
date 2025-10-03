0d4edb5b5f465297fd4f967c16fd15933798fae0
JomarMonreal <jpmonreal2035@gmail.com>
2025-10-04 04:56:00 +0800

    feat: Temporary gamplay flow

--- Objects/Fruit/fruit.gd
+++ Objects/Fruit/fruit.gd
@@ -2,25 +2,24 @@ extends Area2D
 class_name Fruit
 
 @export var node_color: ColorController.GameColor = ColorController.GameColor.WHITE
-@export var texture: Texture2D
-
 @onready var sprite = $Sprite2D
 var was_eaten = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	sprite.texture = texture
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func eat():
-	if not was_eaten:
-		was_eaten = true
-		$AnimationPlayer.play("enlarge")
-		await $AnimationPlayer.animation_finished
-		change_scene()
-		queue_free()
+	if node_color in Fruits.fruit_path_map:
+		var next_scene = Fruits.fruit_path_map[node_color]
+		if next_scene and not was_eaten:
+			was_eaten = true
+			$AnimationPlayer.play("enlarge")
+			await $AnimationPlayer.animation_finished
+			change_scene()
+	queue_free()
 		
 func change_scene() -> void:
 	if node_color in Fruits.fruit_path_map:
