d904e5065271ea36fefac569ac0f214cae83409a
cocoman043 <rocampita@up.edu.ph>
2025-10-04 13:38:27 +0800

    feat: fast music

--- Levels/mystic_forest.gd
+++ Levels/mystic_forest.gd
@@ -11,3 +11,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_arena_area_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
